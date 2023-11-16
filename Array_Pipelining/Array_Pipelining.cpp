56d69a0b7bc8c4ae6033651e48433da16ed4f5f7
TeMbl4 <artemgalkin426226@gmail.com>
2023-11-16 17:42:18 +0300

    16.11

--- Array_Pipelining/Array_Pipelining.cpp
+++ Array_Pipelining/Array_Pipelining.cpp
@@ -4,13 +4,13 @@
 
 using namespace std;
 
-void multiplyArray(double array[], int size, int multiplier) { // 5
+void multiplyArray(double array[], int size, int multiplier) {
     for (int i = 0; i < size; ++i) {
         array[i] *= multiplier;
     }
 }
 
-void addValueToAll(double array[], int size, int index) { // 8
+void addValueToAll(double array[], int size, int index) {
     if ((index >= 0) && (index < size)) {
         int valueToAdd = array[index];
 
@@ -20,24 +20,24 @@ void addValueToAll(double array[], int size, int index) { // 8
     }
 }
 
-void powerArray(double array[], int size, int exponent) { // 20
+void powerArray(double array[], int size, int exponent) {
     for (int i = 0; i < size; ++i) {
         array[i] = pow(array[i], exponent);
     }
 }
 
-void OneToMultiples(double array[], int size, int divisor) { // 47
-    for (int i = 0; i < size; i += size)
-        array[i] = 1;
+void OneToMultiples(double* arr, int par, int size) {
+    for (int i = 0; i < size; i += par)
+        arr[i] = 1;
 }
 
-void changeSignFromIndex(double array[], int size, int index) { // 500
+void changeSignFromIndex(double array[], int size, int index) {
     for (int i = index; i < size; ++i) {
         array[i] = -array[i];
     }
 }
 
-void divideElementAtIndex(double array[], int size, int index) { // 777
+void divideElementAtIndex(double array[], int size, int index) {
     if (index >= 0 && index < size) {
         array[index] /= 2;
     }
@@ -49,57 +49,50 @@ void printArray(double array[], int size) {
     }
 }
 
-void Operation(double array[], int operations[], int params[], bool print_codes[], void (*funcPtrs[6]) (double* array, int, int), int num, int array_size) {
-    for (int i = 0; i < num; i++) {
-        cout << "operation #" << i + 1 << "... ";
-        switch (operations[i]) {
-        case 5:
-            multiplyArray(array, array_size, params[i]);
-            cout << "completed" << endl;
-            break;
-        case 8:
-            addValueToAll(array, array_size, params[i]);
-            cout << "completed" << endl;
-            break;
-        case 20:
-            powerArray(array, array_size, params[i]);
-            cout << "completed" << endl;
-            break;
-        case 47:
-            OneToMultiples(array, array_size, params[i]);
-            cout << "completed" << endl;
-            break;
-        case 500:
-            changeSignFromIndex(array, array_size, params[i]);
-            cout << "completed" << endl;
-            break;
-        case 777:
-            divideElementAtIndex(array, array_size, params[i]);
-            cout << "completed" << endl;
-            break;
-        default:
-            cout << "skipped" << endl;
-            continue;
-        }
+void (*Operation_array(int num)) (double*, int, int) {
+    switch (num) {
+    case 5:
+        return multiplyArray;
+    case 8:
+        return addValueToAll;
+    case 20:
+        return powerArray;
+    case 47:
+        return OneToMultiples;
+    case 500:
+        return changeSignFromIndex;
+    case 777:
+        return divideElementAtIndex;
+    default:
+        return nullptr;
+    }
+}
 
-        if (print_codes[i]) {
-            cout << "Temp data:" << endl;
-            printArray(array, array_size);
+void processArray(double array[], int array_size, int num_of_operations, int params[], bool print_codes[], void (**selectedfuncPtrs) (double*, int, int)) {
+    for (int i = 0; i < num_of_operations; i++) {
+        cout << "operation #" << i + 1 << "... ";
+        if (selectedfuncPtrs[i] != nullptr) {
+            selectedfuncPtrs[i](array, array_size, params[i]);
+            cout << "completed" << endl;
+
+            if (print_codes[i]) {
+                cout << "Temp data:" << endl;
+                printArray(array, array_size);
+            }
+        }
+        else {
+            cout << "skipped" << endl;
         }
     }
 }
 
-
-void main() {
+int main() {
     int array_size, num_of_operations;
-    char print;
-
-    void (*funcPtrs[6]) (double* array, int, int) = { multiplyArray, addValueToAll, powerArray, OneToMultiples, changeSignFromIndex, divideElementAtIndex };
 
     cout << "Enter array size: ";
     cin >> array_size;
 
-    double* array = new double[array_size]; 
+    double* array = new double[array_size];
 
     for (int i = 0; i < array_size; i++) {
         cout << i + 1 << ") ";
@@ -109,26 +102,30 @@ void main() {
     cout << "Enter number of operations: ";
     cin >> num_of_operations;
 
-    int* operations = new int[num_of_operations];
     int* params = new int[num_of_operations];
     bool* print_codes = new bool[num_of_operations];
+    void(**selectedfuncPtrs)(double*, int, int) = new (void(*[num_of_operations])(double*, int, int));
 
     for (int i = 0; i < num_of_operations; ++i) {
+        int num_of_operation;
+        char print;
+
         cout << "(code \\ param \\ print ? ) " << i + 1 << ") ";
-        cin >> operations[i];
-        cin >> params[i];
-        cin >> print;
+        cin >> num_of_operation >> params[i] >> print;
         print_codes[i] = (print == '+');
+
+        selectedfuncPtrs[i] = Operation_array(num_of_operation);
     }
 
     cout << "Processing..." << endl;
-    Operation(array, operations, params, print_codes, funcPtrs, num_of_operations, array_size);
+    processArray(array, array_size, num_of_operations, params, print_codes, selectedfuncPtrs);
 
     cout << "-Result-" << endl;
     printArray(array, array_size);
 
     delete[] array;
-    delete[] operations;
     delete[] params;
     delete[] print_codes;
+
+    return 0;
 }
